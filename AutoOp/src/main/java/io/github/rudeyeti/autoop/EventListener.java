4a5a0c4fb29d13522e35c9fa9d60125f0199c43e
rudeyeti <67293473+RudeYetiIncorporated@users.noreply.github.com>
2020-11-11 09:40:20 -0500

    AutoOp now cancels the deop event; fancy BuildTheEarth text as well.

--- AutoOp/src/main/java/io/github/rudeyeti/autoop/EventListener.java
+++ AutoOp/src/main/java/io/github/rudeyeti/autoop/EventListener.java
@@ -3,6 +3,7 @@ package io.github.rudeyeti.autoop;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
 
 public class EventListener implements Listener {
@@ -16,6 +17,14 @@ public class EventListener implements Listener {
     public void playerJoinEvent(PlayerJoinEvent event) {
         if (!event.getPlayer().isOp()) {
             event.getPlayer().setOp(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = false)
+    public void playerCommandPreprocessEvent(PlayerCommandPreprocessEvent event) {
+        String[] command = event.getMessage().split(" ");
+        if (command[0].equalsIgnoreCase("/deop")) {
+            event.setCancelled(true);
         }
     }
 }
